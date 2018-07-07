5b99b203dd415f0c3a9409a5379b8471d8a22c4a
nice_shot <shefferu@gmail.com>
2018-07-07 23:20:51 +0300

    Added script to fix camera size on phones with different aspect ratios

---
+++ FlickJumpUnity/Assets/CameraFixedRatio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFixedRatio : MonoBehaviour {
+
+    public float targetHeight = 16f;
+    public float targetWidth = 9f;
+
+	void Start () {
+        Camera cam = GetComponent<Camera>();
+
+
+        float targetRatio = targetWidth / targetHeight;
+        float currentRatio = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = currentRatio / targetRatio;
+
+
+        // Solution based on: http://gamedesigntheory.blogspot.com/2010/09/controlling-aspect-ratio-in-unity.html
+        cam.orthographicSize /= scaleHeight;
+    }
+}
