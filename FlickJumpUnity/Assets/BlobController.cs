3bba37b2ed42da94c1cd90b8ff3477a8bd5615a9
nice_shot <shefferu@gmail.com>
2018-07-08 00:08:14 +0300

    Dead positions work!

--- FlickJumpUnity/Assets/BlobController.cs
+++ FlickJumpUnity/Assets/BlobController.cs
@@ -7,6 +7,9 @@ public class BlobController : MonoBehaviour {
     public Vector2 initialDirection;
     public float jumpSpeed;
 
+    private bool isDead = false;
+    private const float SCREEN_WIDTH = 22;
+
     // Components
     private Rigidbody2D body;
     private Animator animator;
@@ -18,11 +21,11 @@ public class BlobController : MonoBehaviour {
     private int animFacingUp = Animator.StringToHash("FacingUp");
     private int animJump = Animator.StringToHash("Jump");
     private int animHitWall = Animator.StringToHash("HitWall");
+    private int animDead = Animator.StringToHash("Dead");
 
     // Visual varriables
     private bool facingRight;
     private bool facingUp;
-    
     
     // Drag variables
     private bool onWall = false;
@@ -46,11 +49,17 @@ public class BlobController : MonoBehaviour {
     }
 	
 	void Update () {
+        if (isDead) {
+            return;
+        }
+
 		if (Input.GetMouseButtonDown(0)) {
             touchStart = (Vector2)Input.mousePosition;
             touchEnd = touchStart;
-            animator.ResetTrigger(animJump);
             animator.SetBool(animPreparing, true);
+            // Used to fix bugs with fast animation transitions
+            animator.ResetTrigger(animJump);
+            animator.ResetTrigger(animHitWall);
         }
 
         if (Input.GetMouseButtonUp(0)) {
@@ -75,20 +84,45 @@ public class BlobController : MonoBehaviour {
 	}
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        // Creating an array to safely get the contact point without creating garbage
+        ContactPoint2D[] contactPoints = new ContactPoint2D[1];
+        collision.GetContacts(contactPoints);
+        facingRight = contactPoints[0].point.x < transform.position.x;
+
         if (collision.transform.CompareTag("Wall")) {
             animator.SetTrigger(animHitWall);
             body.velocity = Vector2.zero;
             sprite.transform.rotation = Quaternion.identity;
 
-            // Creating an array to safely get the contact point without creating garbage
-            ContactPoint2D[] contactPoints = new ContactPoint2D[1];
-            collision.GetContacts(contactPoints);
-
             // Change sprite direction
-            facingRight = contactPoints[0].point.x < transform.position.x;
             sprite.flipX = facingRight;
 
             onWall = true;
+        }
+
+        if (collision.transform.CompareTag("Spike")) {
+            isDead = true;
+            animator.SetBool(animDead, true);
+            body.velocity = Vector2.zero;
+            body.simulated = false;
+            sprite.transform.rotation = Quaternion.identity;
+
+            if (body.position.x > (SCREEN_WIDTH / 2f)) {
+                // Dead right
+                sprite.flipX = false;
+            } else if (body.position.x < -(SCREEN_WIDTH / 2f)) {
+                // Dead left
+                sprite.flipX = true;
+            } else if (body.position.y < contactPoints[0].point.y) {
+                // Dead up
+                sprite.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                sprite.flipX = false;
+            } else {
+                // Dead down
+                sprite.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+                sprite.flipX = false;
+            }
+
         }
     }
 }
