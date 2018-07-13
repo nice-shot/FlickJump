27464498770a06ef08cd4a530553c5f9fe507048
nice-shot <shefferu@gmail.com>
2018-07-13 13:19:34 +0300

    added scripts for constants and angle. Gonna rewrite the blob controller

---
+++ FlickJumpUnity/Assets/Scripts/JumpAngleDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAngleDisplay : MonoBehaviour {
+
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Show(Vector2 touchStart, Direction facingDirection,
+                     float angleLimit, bool useShortGraphic=false) {
+        Debug.Log("Showing arrow");
+    }
+
+    public void Hide() {
+        Debug.Log("Hiding arrow");
+    }
+}
