465da2af7e760d4847fb7c71dc51a40a08781a2c
nice-shot <shefferu@gmail.com>
2018-07-13 13:39:20 +0300

    Added new script for blob controller (will be deleted when changes are placed)

--- FlickJumpUnity/Assets/Scripts/Constants.cs
+++ FlickJumpUnity/Assets/Scripts/Constants.cs
@@ -1,3 +1,7 @@
 public enum Direction {
     UP, DOWN, LEFT, RIGHT
 }
+
+public enum BlobState {
+    ON_WALL, JUMPING, DEAD
+}
---
+++ FlickJumpUnity/Assets/Scripts/NewBlobController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+
+public class BlobControllerNew : MonoBehaviour {
+    // Public parameters
+    public Direction initialDirection;
+    public float jumpAngleLimit;
+    public float initialJumpAngleLimit;
+
+    // States
+    private Direction currentDirection;
+    private BlobState currentState;
+    private bool firstJump = true;
+
+    // Components
+    private Animator animator;
+    private JumpAngleDisplay angleDisplay;
+
+    // Animation params
+    private int animPreparing = Animator.StringToHash("Preparing");
+
+    // Flick Variables
+    private Vector2 touchStart;
+    private Vector2 touchEnd;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+        angleDisplay = GetComponent<JumpAngleDisplay>();
+    }
+
+    void Update() {
+        if (currentState == BlobState.DEAD) {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            // Set touch position
+            touchStart = (Vector2)Input.mousePosition;
+            touchEnd = touchStart;
+            // Set animation preparing
+            animator.SetBool(animPreparing, true);
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            
+        }
+    }
+
+    // Called via the animation so it'll only displayed when on the wall
+    public void ShowAngleDisplay() {
+
+        angleDisplay.Show(touchStart, currentDirection, jumpAngleLimit); 
+    }
+}
