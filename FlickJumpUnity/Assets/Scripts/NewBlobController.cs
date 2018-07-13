dbbb99db0807ab80978b9984cd6fcc9220d85a7d
nice-shot <shefferu@gmail.com>
2018-07-13 17:37:51 +0300

    Direction facing function is working well

--- FlickJumpUnity/Assets/Scripts/NewBlobController.cs
+++ FlickJumpUnity/Assets/Scripts/NewBlobController.cs
@@ -19,10 +19,13 @@ public class NewBlobController : MonoBehaviour {
     // Components
     private Animator animator;
     private Rigidbody2D body;
+    private SpriteRenderer sprite;
     private JumpAngleDisplay angleDisplay;
 
     // Animation params
     private int animPreparing = Animator.StringToHash("Preparing");
+    private int animJumping = Animator.StringToHash("Jumping");
+    private int animDied = Animator.StringToHash("Died");
 
     // Flick Variables
     private Vector2 touchStart;
@@ -32,6 +35,7 @@ public class NewBlobController : MonoBehaviour {
     void Awake() {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
         angleDisplay = GetComponent<JumpAngleDisplay>();
 
         // Drag distnace is 15% height of screen
@@ -51,8 +55,47 @@ public class NewBlobController : MonoBehaviour {
         // Allows starting with jump or with sticking to wall
         if (currentState == BlobState.JUMPING) {
             animator.Play("Blob_Jump");
+            animator.SetBool(animJumping, true);
         } else {
             animator.Play("Blob_OnWall");
+            animator.SetBool(animJumping, false);
+        }
+
+        ChangeSpriteDirection();
+    }
+
+    private void ChangeSpriteDirection() {
+        // When jumping we don't care about the direction state
+        if (currentState == BlobState.JUMPING) {
+            float jumpAngle = Vector2.SignedAngle(Vector2.up, body.velocity);
+            if (jumpAngle > 0f) {
+                sprite.flipX = true;
+                sprite.transform.rotation = Quaternion.Euler(0f, 0f, jumpAngle + 90f);
+            } else {
+                sprite.flipX = false;
+                sprite.transform.rotation = Quaternion.Euler(0f, 0f, jumpAngle + -90f);
+            }
+            return;
+        }
+
+        // This happens on walls and when dead
+        switch (currentDirection) {
+            case Direction.LEFT:
+                sprite.flipX = false;
+                sprite.transform.rotation = Quaternion.identity;
+                break;
+            case Direction.RIGHT:
+                sprite.flipX = true;
+                sprite.transform.rotation = Quaternion.identity;
+                break;
+            case Direction.UP:
+                sprite.flipX = false;
+                sprite.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+                break;
+            case Direction.DOWN:
+                sprite.flipX = false;
+                sprite.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                break;
         }
     }
 
