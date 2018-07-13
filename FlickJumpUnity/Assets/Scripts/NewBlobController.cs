a16b53695bc66e9a9daa2aa0008ad4d52c8d2523
nice-shot <shefferu@gmail.com>
2018-07-13 17:01:49 +0300

    testing new blob controller on stage 3. Added new animation controller as well

--- FlickJumpUnity/Assets/Scripts/NewBlobController.cs
+++ FlickJumpUnity/Assets/Scripts/NewBlobController.cs
@@ -1,19 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class BlobControllerNew : MonoBehaviour {
+public class NewBlobController : MonoBehaviour {
     // Public parameters
-    public Direction initialDirection;
+    public float jumpSpeed;
     public float jumpAngleLimit;
-    public float initialJumpAngleLimit;
+    public float firstJumpAngleLimit;
+    public Vector2 initialVelocity;
+    public BlobState initialState;
+    public Direction initialDirection;
 
     // States
+    private bool firstJump;
+    private Vector3 startingPosition;
     private Direction currentDirection;
     private BlobState currentState;
-    private bool firstJump = true;
 
     // Components
     private Animator animator;
+    private Rigidbody2D body;
     private JumpAngleDisplay angleDisplay;
 
     // Animation params
@@ -22,11 +27,35 @@ public class BlobControllerNew : MonoBehaviour {
     // Flick Variables
     private Vector2 touchStart;
     private Vector2 touchEnd;
+    private float minDragDistance;
 
     void Awake() {
+        body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         angleDisplay = GetComponent<JumpAngleDisplay>();
+
+        // Drag distnace is 15% height of screen
+        minDragDistance = Mathf.Pow((Screen.height * 15 / 100), 2);
+
+        startingPosition = transform.position;
+        Restart();
     }
+
+    private void Restart() {
+        body.position = startingPosition;
+        body.velocity = initialVelocity.normalized * jumpSpeed;
+        currentState = initialState;
+        currentDirection = initialDirection;
+        firstJump = true;
+
+        // Allows starting with jump or with sticking to wall
+        if (currentState == BlobState.JUMPING) {
+            animator.Play("Blob_Jump");
+        } else {
+            animator.Play("Blob_OnWall");
+        }
+    }
+
 
     void Update() {
         if (currentState == BlobState.DEAD) {
@@ -42,13 +71,12 @@ public class BlobControllerNew : MonoBehaviour {
         }
 
         if (Input.GetMouseButtonUp(0)) {
-            
+            animator.SetBool(animPreparing, false);
         }
     }
 
     // Called via the animation so it'll only displayed when on the wall
     public void ShowAngleDisplay() {
-
         angleDisplay.Show(touchStart, currentDirection, jumpAngleLimit); 
     }
 }
